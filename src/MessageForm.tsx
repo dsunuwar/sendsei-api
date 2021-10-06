f067cdb2994a95c7e18fcf07a69d5cf14836c83a
Full Name <Git Email>
2021-10-07 08:05:13 +1100

    add form fields, style and basic validation

--- src/MessageForm.tsx
+++ src/MessageForm.tsx
@@ -1,11 +1,72 @@
 import React from "react";
+import './MessageForm.css';
 
 interface Props {}
 
 const MessageForm: React.FC<Props> = () => {
+  const [recipient, setRecipient] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  const [formValid, setFormValid] = React.useState(false);
+
+  // assume max length of single sms text is 160 chars
+  const MESSAGE_MAX_LENGTH = 3 * 160;
+
+  const validateMessage = (e: any) => {
+    const {name, value} = e.target;
+    if(name === 'message' && value.length > MESSAGE_MAX_LENGTH) {
+      setFormValid(true);
+    } else {
+      setFormValid(false);
+    }
+    setMessage(value);
+  }
+  
+  const handleSubmit = (e: React.FormEvent)  => {
+    e.preventDefault();
+    if(formValid) {
+      console.log('form submitted');
+      setFormValid(false);
+    }
+  };
+
   return (
     <div className="MessageForm">
       <header>Send SMS</header>
+      <form noValidate onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="">Sender:</label>
+          <input 
+            readOnly
+            id="sender" 
+            name="sender"
+            type="text" 
+            value="Enterprise" />
+        </div>
+        <div>
+          <label htmlFor="recipient">Recipient:</label>
+          <input 
+              id="recipient"
+              name="recipient"
+              type="text" 
+              placeholder="Recipent name"
+              value={recipient}
+              onChange={(e) => setRecipient(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea 
+              id="message"
+              name="message"
+              placeholder="Message"
+              rows={5}
+              cols={17}
+              value={message}
+              onChange={(e) => validateMessage(e)}
+              />
+        </div>        
+        <input type="submit" value="Submit" disabled={!formValid}/>
+      </form>
     </div>
   );
 };
