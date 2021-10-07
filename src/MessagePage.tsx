772b6bcdd57127a037b29410319703bae2c91932
Full Name <Git Email>
2021-10-07 22:55:54 +1100

    updated message page to store sent messages

--- src/MessagePage.tsx
+++ src/MessagePage.tsx
@@ -3,11 +3,25 @@ import React from "react";
 import MessageDisplay from "./MessageDisplay";
 import MessageForm from "./MessageForm";
 
+export type Message = {
+  cost: number,
+  message: string,
+  recipient: string,
+}
+
 const MessagePage = () => {
+  const [sentMessages, setSentMessages] = React.useState<Message[]>([]);
+
+  const updateMessage = (sms: Message) => {
+    if(sms) {
+      setSentMessages([...sentMessages, sms]);
+    }
+  };
+
   return (
     <div className="MessagePage">
-      <MessageForm />
-      <MessageDisplay />
+      <MessageForm updateMessage={updateMessage}/>
+      <MessageDisplay sentMessages={sentMessages}/>
     </div>
   );
 };
