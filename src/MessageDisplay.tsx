8824018e14f27dd762d6f9fa7ac58521ae5a5d14
Full Name <Git Email>
2021-10-07 22:54:25 +1100

    list sent messages

--- src/MessageDisplay.tsx
+++ src/MessageDisplay.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-interface Props {}
+type Message = {
+  cost: number,
+  message: string,
+  recipient: string,
+}
 
-const MessageDisplay: React.FC<Props> = () => {
+interface Props {
+  sentMessages: Array<Message>
+}
+
+const MessageDisplay: React.FC<Props> = ({ sentMessages=[] }) => {
   return (
     <div className="MessageDisplay">
       <header>Sent SMS</header>
+      <ul>
+        {sentMessages.map(({ cost, message, recipient }, index) => (
+          <React.Fragment key={`${index}recipient`}>
+            <li>
+              {`Cost: ${cost}, Message: ${message}, Recipient: ${recipient}`}
+            </li>
+          </React.Fragment>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default MessageDisplay;
-
